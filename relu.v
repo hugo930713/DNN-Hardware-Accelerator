2298ee3c7267d618113ac8229cbd0eaa64fd477c
hugo930713 <hugo930713@gmail.com>
2025-06-11 15:38:34 +0800

    Rename input/output ports from din/dout to data_in/data_out in relu module

--- relu.v
+++ relu.v
@@ -3,8 +3,8 @@ module relu #(parameter WIDTH_IN = 16, WIDTH_OUT = 8) (
     input clk,
     input rst_n,
     input valid_in,
-    input  signed [WIDTH_IN-1:0] din,
-    output reg [WIDTH_OUT-1:0] dout,
+    input  signed [WIDTH_IN-1:0] data_in,
+    output reg [WIDTH_OUT-1:0] data_out,
     output reg valid_out
   );
 
@@ -12,22 +12,22 @@ module relu #(parameter WIDTH_IN = 16, WIDTH_OUT = 8) (
   begin
     if (!rst_n)
     begin
-      dout <= 0;
+      data_out <= 0;
       valid_out <= 0;
     end
     else if (valid_in)
     begin
       // ReLU 函數處理
-      if (din > 0)
+      if (data_in > 0)
       begin
-        if (din > 127)   // Saturate to int8 最大值
-          dout <= 8'd127;
+        if (data_in > 127)   // Saturate to int8 最大值
+          data_out <= 8'd127;
         else
-          dout <= din[7:0];  // din 保證在 0~127 之間，取低 8 位沒問題
+          data_out <= data_in[7:0];  // data_in 保證在 0~127 之間，取低 8 位沒問題
       end
       else
       begin
-        dout <= 8'd0;
+        data_out <= 8'd0;
       end
 
       valid_out <= 1;  // 當 valid_in 有效時，valid_out 設為 1
