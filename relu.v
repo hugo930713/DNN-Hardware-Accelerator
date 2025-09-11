db3a4bfdeb95e9c9b50927800959791c452cdb34
hugo930713 <hugo930713@gmail.com>
2025-09-11 15:26:29 +0800

    fix(relu): 將輸出結果的位寬從8位改為16位，以支持更大的數值範圍

--- relu.v
+++ relu.v
@@ -1,10 +1,10 @@
 // relu.v
-module relu #(parameter WIDTH_IN = 16, WIDTH_OUT = 8) (
+module relu #(parameter WIDTH_IN = 16, WIDTH_OUT = 16) (
     input clk,
     input rst_n,
     input valid_in,
-    input  signed [WIDTH_IN-1:0] data_in,
-    output reg [WIDTH_OUT-1:0] data_out,
+    input  signed [15:0] data_in,
+    output reg signed [15:0] data_out,
     output reg valid_out
   );
 
@@ -20,14 +20,14 @@ module relu #(parameter WIDTH_IN = 16, WIDTH_OUT = 8) (
       // ReLU 函數處理
       if (data_in > 0)
       begin
-        if (data_in > 127)   // Saturate to int8 最大值
-          data_out <= 8'd127;
+        if (data_in > 16'sh7FFF)   // Q8.8最大值
+          data_out <= 16'sh7FFF;
         else
-          data_out <= data_in[7:0];  // data_in 保證在 0~127 之間，取低 8 位沒問題
+          data_out <= data_in;
       end
       else
       begin
-        data_out <= 8'd0;
+        data_out <= 16'd0;
       end
 
       valid_out <= 1;  // 當 valid_in 有效時，valid_out 設為 1
